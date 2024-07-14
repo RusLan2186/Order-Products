bb00dc7d090e61de9eb565b7829d12cb975ee6ab
Ruslan <4irus86@gmail.com>
2024-07-14 11:44:12 +0300

    install github-pages

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
-  // server: {
-  //   watch: {
-  //     usePolling: true
-  //   },
-  //   host: true,
-  //   strictPort: true,
-  //   port: 5173
-  // },
+    base:  '/Order-Products/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
-})
+  },
+
+});
